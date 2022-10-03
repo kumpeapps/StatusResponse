8d264f7edfb57e449f57d0aa6d679e961ed24808
Justin Kumpe <jakumpe@kumpes.com>
2022-10-03 15:00:51 -0500

    Initial Commit

---
+++ StatusResponse/StatusResponse.h
@@ -0,0 +1,18 @@
+//
+//  StatusResponse.h
+//  StatusResponse
+//
+//  Created by Justin Kumpe on 10/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StatusResponse.
+FOUNDATION_EXPORT double StatusResponseVersionNumber;
+
+//! Project version string for StatusResponse.
+FOUNDATION_EXPORT const unsigned char StatusResponseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StatusResponse/PublicHeader.h>
+
+
